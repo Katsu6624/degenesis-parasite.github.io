357727cc5648690db0e54ba6affdcec1e7194a1f
Katsu6624 <167237949+Katsu6624@users.noreply.github.com>
2026-06-15 02:27:50 +0200

    fix: valeur stat en blanc comme le label (sans opacité réduite)

--- src/components/StatusMonitor.vue
+++ src/components/StatusMonitor.vue
@@ -8,7 +8,7 @@
       <ValueBoxes class="text-caption" :count="max" :value="value" :interactive="false" />
     </div>
     <div class="d-flex justify-center mb-3 text-uppercase">
-      {{ label }} <span style="opacity:0.5;margin-left:4px">({{ value }})</span>
+      {{ label }} <span style="margin-left:4px">({{ value }})</span>
     </div>
     <v-tooltip
       v-if="tooltip.length > 0"
